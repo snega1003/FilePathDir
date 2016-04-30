f340ab26ed1141eb7d00ce0e2088814ca0806e1b
snega1003 <snega900@gmail.com>
2016-04-30 19:01:36 +0300

    Project
    Classes, 2 hash

---
+++ FoundFilesInDirWithClasses.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-03-19T20:53:27
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = FoundFilesInDirWithClasses
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    dialog.cpp
+
+HEADERS  += mainwindow.h \
+    dialog.h
+
+FORMS    += mainwindow.ui \
+    dialog.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,132 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <dialog.h>
+#include <QIODevice>
+#include <QDebug>
+#include <QtGui>
+#include <QString>
+#include <QCryptographicHash>
+#include <QMessageBox>
+#include <vector>
+
+class Files {
+  public:
+    QFileInfo file;
+    QString absolutepath;
+    QString hash1;
+    QString hash2;
+    QString hash3;
+    QString filename;
+};
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionPrint_directory_triggered()
+{
+    Dialog *wd = new Dialog(this);
+    wd->show();
+    connect(wd,SIGNAL(FilePath(QString)),this,SLOT(ReadPath(QString)));
+}
+
+
+
+
+QString ReadPath2(QString path,int level, QFile*  mFile,int k){
+    int check = 1;
+    QString space;
+    for(int i=1; i<= level; i++)
+        space+="             ";
+    QDir folder(path);
+    QString buffer;
+    QTextStream stream(mFile);
+    QFileInfo pFile(*mFile);
+    if(!folder.exists()) {
+        //qDebug() << "Такой дериктории не существует. Введите путь заново. " + '\n';
+        QMessageBox msgBox;
+        msgBox.setText("У вас почти получилось!");
+        msgBox.exec();
+        buffer = buffer + "Такой дериктории не существует. Введите путь заново. "+ '\n';
+        stream << buffer;
+    }
+
+    if(k==1) {
+        buffer = buffer + "Файл, описывающий дерикторию -> " + pFile.absoluteFilePath() + '\n'+ '\n'+ '\n';
+        k++;
+    }
+
+    for(QFileInfo temp : folder.entryInfoList())
+    {
+        if(check > 2 )
+        {
+            Files fileindir;
+            fileindir.file = temp;
+            fileindir.absolutepath = temp.absolutePath();
+            fileindir.filename = temp.fileName();
+
+            QFile rem(temp.absoluteFilePath());
+            rem.open(QIODevice::ReadOnly);
+            QString hashMd5 = QCryptographicHash::hash(rem.readAll(), QCryptographicHash::Md5).toHex().constData();
+            QString hashSha256 = QCryptographicHash::hash(rem.readAll(), QCryptographicHash::Sha256).toHex().constData();
+            fileindir.hash1 = hashMd5;
+            fileindir.hash2 = hashSha256;
+            if(temp.isDir())
+                buffer = buffer + ReadPath2(temp.absoluteFilePath(),++level, mFile,k++);
+
+
+
+             buffer = buffer + space + fileindir.absolutepath +"         " + fileindir.filename +'\n'
+                    + "     хеш файла в Md5: " + fileindir.hash1 +  '\n' +
+                     "     хеш файла в Sha256: " + fileindir.hash2 + '\n';
+             rem.close();
+         }
+        check++;
+        stream << buffer;
+    }
+
+    return buffer;
+}
+
+
+void MainWindow::ReadPath(const QString &filePath) {
+    QFile mFile("mFile.txt");
+    mFile.open(QIODevice::WriteOnly);
+    //QFileInfo pFile(&mFile);
+
+    ui->textEdit->setText(ReadPath2(filePath,0,&mFile,1));
+    mFile.close();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
