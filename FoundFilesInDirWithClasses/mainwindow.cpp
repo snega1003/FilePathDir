3ae92f7ed75cd18aacd1d045daeeff4a00451d2c
bulgakovav <vikysjab@mail.ru>
2016-05-13 21:49:31 +0300

    Upgrade
    serializatoin, file etc

--- FoundFilesInDirWithClasses/mainwindow.cpp
+++ FoundFilesInDirWithClasses/mainwindow.cpp
@@ -12,30 +12,9 @@
 #include <QCryptographicHash>
 #include <QMessageBox>
 #include <vector>
-#include <boost_1_60_0/boost/serialization/serialization.hpp>
-
-class Files {
- public:
-    QFileInfo file;
-    QString absolutepath;
-    QString hash1;
-    QString hash2;
-    QString hash3;
-    QString filename;
-
- template<class Archive>
-    void serialize(Archive & ar, const unsigned int version)
-     {
-         ar & file;
-         ar & absolutepath;
-         ar & hash1;
-         ar & hash2;
-         ar & hash3;
-         ar & filename;
-    }
-};
-
-
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -57,10 +36,31 @@ void MainWindow::on_actionPrint_directory_triggered()
     connect(wd,SIGNAL(FilePath(QString)),this,SLOT(ReadPath(QString)));
 }
 
+class Files {
+  public:
+    QFileInfo file;
+    QString absolutepath;
+    QString hash1;
+    QString hash2;
+    QString hash3;
+    QString filename;
+    //int size;
+
+    template <class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & file;
+        ar & absolutepath;
+        ar & hash1;
+        ar & hash2;
+        ar & hash3;
+        ar & filename;
+        ar & size;
+    }
+};
 
 
-
-QString ReadPath2(QString path,int level, QFile*  mFile,int k){
+QString ReadPath2(QString path,int level, QFile*  mFile,int k ){
     int check = 1;
     QString space;
     for(int i=1; i<= level; i++)
@@ -68,24 +68,20 @@ QString ReadPath2(QString path,int level, QFile*  mFile,int k){
     QDir folder(path);
     QString buffer;
     QTextStream stream(mFile);
-    boost::archive::text_oarchive serializer(stream);
     QFileInfo pFile(*mFile);
     if(!folder.exists()) {
-        //qDebug() << "Такой дериктории не существует. Введите путь заново. " + '\n';
         QMessageBox msgBox;
         msgBox.setText("У вас почти получилось!");
         msgBox.exec();
         buffer = buffer + "Такой дериктории не существует. Введите путь заново. "+ '\n';
-        serializer << buffer;
         stream << buffer;
-
     }
 
     if(k==1) {
         buffer = buffer + "Файл, описывающий дерикторию -> " + pFile.absoluteFilePath() + '\n'+ '\n'+ '\n';
         k++;
+        stream << buffer;
     }
-
     for(QFileInfo temp : folder.entryInfoList())
     {
         if(check > 2 )
@@ -94,6 +90,7 @@ QString ReadPath2(QString path,int level, QFile*  mFile,int k){
             fileindir.file = temp;
             fileindir.absolutepath = temp.absolutePath();
             fileindir.filename = temp.fileName();
+            //fileindir.size = temp.size();
 
             QFile rem(temp.absoluteFilePath());
             rem.open(QIODevice::ReadOnly);
@@ -101,30 +98,34 @@ QString ReadPath2(QString path,int level, QFile*  mFile,int k){
             QString hashSha256 = QCryptographicHash::hash(rem.readAll(), QCryptographicHash::Sha256).toHex().constData();
             fileindir.hash1 = hashMd5;
             fileindir.hash2 = hashSha256;
-            if(temp.isDir())
+
+
+            if(temp.isDir()){
                 buffer = buffer + ReadPath2(temp.absoluteFilePath(),++level, mFile,k++);
+                stream << buffer;
+            }
+            else {
 
+             buffer = buffer + space + fileindir.absolutepath +"         " + fileindir.filename + /*"  size: " +fileindir.size */ +'\n'
+                     + space +"     хеш файла в Md5: " + fileindir.hash1 + '\n'
+                     + space +"     хеш файла в Sha256: "+ fileindir.hash2 + '\n';
 
-
-             buffer = buffer + space + fileindir.absolutepath +"         " + fileindir.filename +'\n'
-                    + "     хеш файла в Md5: " + fileindir.hash1 +  '\n' +
-                     "     хеш файла в Sha256: " + fileindir.hash2 + '\n';
+             stream << buffer;
+             stream << endl;
+            }
              rem.close();
          }
         check++;
-        serializer << buffer;
-        stream << buffer;
+        //stream << buffer;
     }
 
     return buffer;
 }
 
-
 void MainWindow::ReadPath(const QString &filePath) {
     QFile mFile("mFile.txt");
     mFile.open(QIODevice::WriteOnly);
     //QFileInfo pFile(&mFile);
-
     ui->textEdit->setText(ReadPath2(filePath,0,&mFile,1));
     mFile.close();
 
